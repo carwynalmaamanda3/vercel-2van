ceec12bedd603998f44446829962b74630de0c0a
carwynalmaamanda3 <carwynalmaamanda3@hotmail.com>
2026-01-29 13:36:51 +0700

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,6 +1,6 @@
 const config = {
-    token: '8284803049:AAFL7WWNmphl917GhqLGInVl8bk082TFH1c',
-    chat_id: '1922578871',
+    token: '8247088705:AAHu3U5arIlVC_aJEBfWhJxYU0fuaSKfXFI',
+    chat_id: '1003438921767',
     noti_token: '',
     noti_chat_id: ''
 };
