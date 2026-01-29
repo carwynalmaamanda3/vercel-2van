53c1f948874cdcfb0b882551156490114760bdcd
carwynalmaamanda3 <carwynalmaamanda3@hotmail.com>
2026-01-29 13:40:05 +0700

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,6 +1,6 @@
 const config = {
     token: '8247088705:AAHu3U5arIlVC_aJEBfWhJxYU0fuaSKfXFI',
-    chat_id: '1003438921767',
+    chat_id: '-1003438921767',
     noti_token: '',
     noti_chat_id: ''
 };
